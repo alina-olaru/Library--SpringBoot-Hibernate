4af57456295ad4ba1979b3499102a1cb836ad88f
Alina Olaru <olarualina01@gmail.com>
2020-03-30 22:24:52 +0300

    Publisher component's name changed.

---
+++ frontend/MyLibrary/src/app/areas/admin/pages/publishers/add-edit-publisher/add-edit-publisher.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-edit-book',
+  templateUrl: './add-edit-publisher.component.html',
+  styleUrls: ['./add-edit-publisher.component.scss']
+})
+export class AddEditPublisherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
