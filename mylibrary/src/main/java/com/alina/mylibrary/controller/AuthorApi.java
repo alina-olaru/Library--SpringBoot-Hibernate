0c47305f063d1fd5a733977c7951864e63bd2108
Alina Olaru <olarualina01@gmail.com>
2020-03-30 13:57:37 +0300

    CRUD API's created

--- mylibrary/src/main/java/com/alina/mylibrary/controller/AuthorApi.java
+++ mylibrary/src/main/java/com/alina/mylibrary/controller/AuthorApi.java
@@ -1,4 +1,45 @@
 package com.alina.mylibrary.controller;
 
+
+import com.alina.mylibrary.model.ApiResponse;
+import com.alina.mylibrary.model.Author;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Author")
+@CrossOrigin
 public interface AuthorApi {
+
+    @PostMapping
+    ApiResponse<Author> insertAuthor(@RequestBody Author author);
+
+
+    @GetMapping
+    ApiResponse<List<Author>>  getAuthors();
+
+
+    @PutMapping
+    ApiResponse<Author> updateAuthor(@RequestBody Author author);
+
+
 }
+
+
+
+/*
+
+@RestController
+@RequestMapping("/public/register")
+@CrossOrigin
+public interface RegisterApi {
+
+    @PostMapping("/do")
+    ApiResponse<BookUser> registerUser(@RequestBody BookUser user);
+
+}
+
+
+
+ */
--- mylibrary/src/main/java/com/alina/mylibrary/controller/impl/AutorApiImp.java
+++ mylibrary/src/main/java/com/alina/mylibrary/controller/impl/AutorApiImp.java
@@ -1,4 +1,66 @@
 package com.alina.mylibrary.controller.impl;
 
-public class AutorApiImp {
+import com.alina.mylibrary.controller.AuthorApi;
+import com.alina.mylibrary.model.ApiResponse;
+import com.alina.mylibrary.model.ApiResponseType;
+import com.alina.mylibrary.model.Author;
+import com.alina.mylibrary.service.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class AutorApiImp  implements AuthorApi {
+
+
+    @Autowired
+    private AuthorService authorService;
+
+
+    @Override
+    public ApiResponse<Author> insertAuthor(Author author) {
+        if(author!=null){
+            Author response=this.authorService.addAuthor(author);
+            return new ApiResponse<Author>(ApiResponseType.SUCCESS,response);
+        }
+
+        return new ApiResponse<Author>(ApiResponseType.ERROR,null,"Autorul nu a putut fi adaugat");
+    }
+
+    @Override
+    public ApiResponse<List<Author>> getAuthors() {
+      List<Author> response=this.authorService.getAuthors();
+      return new ApiResponse<List<Author>>(ApiResponseType.SUCCESS,response);
+    }
+
+    @Override
+    public ApiResponse<Author> updateAuthor(Author author) {
+       if(author!=null)
+       {
+           Author response=this.authorService.updateAuthor(author);
+           return new ApiResponse<Author>(ApiResponseType.SUCCESS,author);
+       }
+       return new ApiResponse<Author>(ApiResponseType.ERROR,null,"Autorul nu a putut fi updatat");
+    }
 }
+
+
+
+/*
+
+
+    @Autowired
+    private RegisterService registerService;
+
+    @Override
+    public ApiResponse<BookUser> registerUser(@RequestBody BookUser user) {
+        if(user!=null){
+            BookUser response = this.registerService.registerUser(user);
+            return new ApiResponse<BookUser>(ApiResponseType.SUCCESS, response);
+        }
+        //TODO mesaj eroare cu detalii
+        return new ApiResponse<BookUser>(ApiResponseType.ERROR, null, "Contul nu a putut fi creat, verifica datele introduse!");
+    }
+}
+
+
+ */
