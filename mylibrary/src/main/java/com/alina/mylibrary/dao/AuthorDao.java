33dffa3c97530baf95f97261e18f6f4c95fd7a2a
Alina Olaru <olarualina01@gmail.com>
2020-03-30 13:25:06 +0300

    AuthorDao

--- mylibrary/src/main/java/com/alina/mylibrary/dao/AuthorDao.java
+++ mylibrary/src/main/java/com/alina/mylibrary/dao/AuthorDao.java
@@ -1,4 +1,17 @@
 package com.alina.mylibrary.dao;
 
+import com.alina.mylibrary.model.Author;
+
+import java.util.List;
+
 public interface AuthorDao {
+    public List<Author> getAuthors();
+    public Author getAuthorById(int authorId);
+    public Author getAuthorByFirstName(String firstName);
+    public Author getAuthorByLastName(String LastName);
+    public Author addAuthor(Author author);
+    public boolean deleteAuthor(int authorId);
+    public Author updateAuthor(Author author);
+
+
 }
--- mylibrary/src/main/java/com/alina/mylibrary/repository/BookRepository.java
+++ mylibrary/src/main/java/com/alina/mylibrary/repository/BookRepository.java
@@ -1,13 +1,19 @@
 package com.alina.mylibrary.repository;
 
+import com.alina.mylibrary.model.Author;
 import com.alina.mylibrary.model.Book;
 import com.alina.mylibrary.model.BookUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
 @Repository
 @Transactional
 
 public interface BookRepository extends JpaRepository<Book,Integer> {
+
+
 }
 
