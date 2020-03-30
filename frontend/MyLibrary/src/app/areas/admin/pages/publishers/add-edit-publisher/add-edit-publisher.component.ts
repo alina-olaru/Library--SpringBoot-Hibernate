4c1ed00d4593969e0e921818bfaf461b1cc2ea95
Alina Olaru <olarualina01@gmail.com>
2020-03-30 23:23:19 +0300

    publisher's layout

--- frontend/MyLibrary/src/app/areas/admin/pages/publishers/add-edit-publisher/add-edit-publisher.component.ts
+++ frontend/MyLibrary/src/app/areas/admin/pages/publishers/add-edit-publisher/add-edit-publisher.component.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { Publisher } from 'src/app/Models/admin/PublisherModel';
+import {MatIconModule} from '@angular/material/icon';
+import { Author } from "src/app/Models/admin/AuthorModel";
+import {
+  FormControl,
+  FormGroup,
+  FormBuilder,
+  Validators
+} from "@angular/forms";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+
+
+interface Data {
+  type: string;
+  model: Publisher;
+}
+
+
 
 @Component({
-  selector: 'app-add-edit-book',
+  selector: 'app-add-edit-publisher',
   templateUrl: './add-edit-publisher.component.html',
   styleUrls: ['./add-edit-publisher.component.scss']
 })
 export class AddEditPublisherComponent implements OnInit {
+  localForm: FormGroup;
+  myControl = new FormControl();
 
-  constructor() { }
+
+  constructor(
+    public dialogRef: MatDialogRef<AddEditPublisherComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Data,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
+    if (this.data.model == null || this.data.model == undefined) {
+      this.data.model = new Publisher();
+    }
+    this.localForm = this.formBuilder.group({
+      authorId: [{ value: this.data.model.publisherId, disabled: true }],
+      publisherTitle: [this.data.model.publisherTitle, Validators.required]
+
+    });
   }
 
+  get form() {
+    return this.localForm.controls;
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  DeleteFile() {
+
+    this.data.model.publisherTitle = null;
+
+  }
+
+  SubmitForm() {
+    if (this.localForm.valid) {
+      console.log(this.localForm.value);
+      let model: Publisher = new Publisher(this.localForm.value);
+      model.publisherId = this.data.model.publisherId;
+      this.dialogRef.close(model);
+    }
+  }
 }
---
+++ frontend/MyLibrary/src/app/areas/admin/pages/publishers/publishers.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { GlobalVarService } from 'src/app/services/global-var.service';
+import { ApiResponse } from 'src/app/Models/general/api-response';
+import { Publisher } from 'src/app/Models/admin/PublisherModel';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PublishersService {
+
+  baseUrl:String;
+
+
+  constructor(
+    private http: HttpClient,
+    private globalVarService: GlobalVarService
+  ) {
+    this.baseUrl = "/public/api/edituri";
+
+  }
+
+  GetPublishers() {
+    return this.http.get<ApiResponse<Publisher[]>>(
+      this.globalVarService.globalUrl + this.baseUrl
+    );
+  }
+
+  DeletePublisher(id: number): Observable<ApiResponse<boolean>> {
+    return this.http.delete<ApiResponse<boolean>>(
+      this.globalVarService.globalUrl + this.baseUrl + "/" + id
+    );
+  }
+
+  AddPublisher(publisher: Publisher): Observable<ApiResponse<Publisher>> {
+    return this.http.post<ApiResponse<Publisher>>(
+      this.globalVarService.globalUrl + this.baseUrl,
+      publisher
+    );
+  }
+
+  UpdatePublisher(publisher:Publisher ,id: number): Observable<ApiResponse<Publisher>> {
+    return this.http.put<ApiResponse<Publisher>>(
+      this.globalVarService.globalUrl + this.baseUrl + "/" + id,
+      publisher
+    );
+  }
+}
