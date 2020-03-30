692f8473e9b26221b0101639d49ea7156b7eeb86
Alina Olaru <olarualina01@gmail.com>
2020-03-30 20:13:50 +0300

    Publisher repository.

--- mylibrary/src/main/java/com/alina/mylibrary/repository/PublisherRepository.java
+++ mylibrary/src/main/java/com/alina/mylibrary/repository/PublisherRepository.java
@@ -1,12 +1,19 @@
 package com.alina.mylibrary.repository;
 
+import com.alina.mylibrary.model.Author;
 import com.alina.mylibrary.model.Publisher;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Repository
 @Transactional
 
 public interface PublisherRepository extends JpaRepository<Publisher, Integer> {
+
+   Publisher findrBypublisherTitle(String publisherTitle);
+    Publisher findrBypublisherId(int publisherId);
+
 }
