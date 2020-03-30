69f0f8eaca6b9721aa2a105e56702b9c0b1679d4
Alina Olaru <olarualina01@gmail.com>
2020-03-30 21:47:29 +0300

    Layout updated.

--- frontend/MyLibrary/src/app/areas/admin/pages/publishers/publishers.component.ts
+++ frontend/MyLibrary/src/app/areas/admin/pages/publishers/publishers.component.ts
@@ -1,5 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { TitleService } from './../../services/title.service';
+import { DomSanitizer } from '@angular/platform-browser';
+import { ToastrService } from 'src/app/services/toastr.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { Author } from 'src/app/Models/admin/AuthorModel';
+import { LoadingService } from 'src/app/modules/loading-spinner/loading.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ApiResponse } from 'src/app/Models/general/api-response';
+import { ApiResponseType } from 'src/app/Models/general/api-response-type.enum';
+import { Publisher } from 'src/app/Models/admin/PublisherModel';
 @Component({
   selector: 'app-publishers',
   templateUrl: './publishers.component.html',
@@ -7,9 +18,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PublishersComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild(MatSort, { static: false }) sort: MatSort;
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
 
-  ngOnInit(): void {
-  }
+ publishers:Publisher[] = [];
+ _addPublisher:Publisher;
+  displayedColumns: string[] = ['publisherId', 'publisherTitle', 'actions'];
+  dataSource: MatTableDataSource<Publisher> = new MatTableDataSource(this.publishers);
 
+
+
+  constructor(    private titleService: TitleService,
+    private toastr: ToastrService,
+    private sanitizer: DomSanitizer,
+    private loadingService: LoadingService,
+    public dialog: MatDialog
+ ) { }
+
+ ngOnInit() {
+  this.GetPubishers();
+  this.titleService.setTitle('faGlobeEurope', 'Edituri');
+}
+
+applyFilter(filterValue: string) {
+  this.dataSource.filter = filterValue.trim().toLowerCase();
+}
+
+updateDataSouce() {
+  this.dataSource = new MatTableDataSource(this.publishers);
+  this.dataSource.sort = this.sort;
+  this.dataSource.paginator = this.paginator;
+}
+
+GetPubishers(){
+
+
+}
 }
