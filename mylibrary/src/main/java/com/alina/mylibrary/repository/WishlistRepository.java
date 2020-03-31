29b40fcebeff5fac10e54842edcf60408715722c
Alina Olaru <olarualina01@gmail.com>
2020-03-31 17:16:19 +0300

    find by user and by book ( for admin dashboard)

--- mylibrary/src/main/java/com/alina/mylibrary/repository/WishlistRepository.java
+++ mylibrary/src/main/java/com/alina/mylibrary/repository/WishlistRepository.java
@@ -1,10 +1,14 @@
 package com.alina.mylibrary.repository;
 
+import com.alina.mylibrary.model.Book;
+import com.alina.mylibrary.model.BookUser;
 import com.alina.mylibrary.model.PersonalBook;
 import com.alina.mylibrary.model.Wishlist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 @Repository
@@ -12,5 +16,16 @@ import org.springframework.transaction.annotation.Transactional;
 
 
 public interface WishlistRepository extends JpaRepository<Wishlist, Integer> {
+
+    /*
+     List<BookUser> findByEmailAdress(String emailAdress);
+    List<BookUser> findByUsername(String username);
+     */
+
+    //todo de implementat toate astea pt dashboard + populat cu date pt grafice
+
+    List<Wishlist> findByuserwishlist(BookUser userwishlist);
+    List<Wishlist> findBybookwishlist(Book bookwishlist);
+
 }
 
