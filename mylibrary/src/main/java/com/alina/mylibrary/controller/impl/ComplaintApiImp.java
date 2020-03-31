f107d57d7f7d73104d9f0149efc06d51a0ce4ed2
Alina Olaru <olarualina01@gmail.com>
2020-03-31 17:09:52 +0300

    GetApi

--- mylibrary/src/main/java/com/alina/mylibrary/controller/impl/ComplaintApiImp.java
+++ mylibrary/src/main/java/com/alina/mylibrary/controller/impl/ComplaintApiImp.java
@@ -1,4 +1,25 @@
 package com.alina.mylibrary.controller.impl;
 
-public class ComplaintApiImp {
+import com.alina.mylibrary.controller.ComplaintApi;
+import com.alina.mylibrary.model.ApiResponse;
+import com.alina.mylibrary.model.ApiResponseType;
+import com.alina.mylibrary.model.Complaint;
+import com.alina.mylibrary.service.ComplaintService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class ComplaintApiImp implements ComplaintApi {
+
+    @Autowired
+    ComplaintService complaintService;
+
+
+    @Override
+    public ApiResponse<List<Complaint>> getComplaints() {
+     List<Complaint> response=this.complaintService.getComplaints();
+     return new ApiResponse<List<Complaint>>(ApiResponseType.SUCCESS,response);
+    }
+
+
 }
