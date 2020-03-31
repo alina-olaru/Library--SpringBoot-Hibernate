dcbd6d7114ac656f1aceafbca4c21e2fc14d5329
Alina Olaru <olarualina01@gmail.com>
2020-03-31 17:16:32 +0300

    @Component added(bug fixed)

--- mylibrary/src/main/java/com/alina/mylibrary/dao/impl/CompaintDaoImp.java
+++ mylibrary/src/main/java/com/alina/mylibrary/dao/impl/CompaintDaoImp.java
@@ -4,9 +4,12 @@ import com.alina.mylibrary.dao.ComplaintDao;
 import com.alina.mylibrary.model.Complaint;
 import com.alina.mylibrary.repository.ComplaintRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+
+@Component
 public class CompaintDaoImp implements ComplaintDao {
 
     @Autowired
