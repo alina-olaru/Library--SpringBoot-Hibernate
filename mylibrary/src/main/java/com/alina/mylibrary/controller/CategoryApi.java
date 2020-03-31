b9d1431bd71d0667c8297457e3860a30c7758e00
Alina Olaru <olarualina01@gmail.com>
2020-03-31 11:31:09 +0300

    Api's for category

---
+++ mylibrary/src/main/java/com/alina/mylibrary/controller/CategoryApi.java
@@ -0,0 +1,31 @@
+package com.alina.mylibrary.controller;
+
+
+import com.alina.mylibrary.model.ApiResponse;
+import com.alina.mylibrary.model.Book;
+import com.alina.mylibrary.model.Category;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/public/api/admin/Category")
+@CrossOrigin
+
+
+public interface CategoryApi {
+
+
+    @GetMapping
+    ApiResponse<List<Category>> getCategories();
+
+
+    @PostMapping
+    ApiResponse<com.alina.mylibrary.model.Category> insertCategory(@RequestBody com.alina.mylibrary.model.Category category);
+
+    @PutMapping(path="/{id}")
+    ApiResponse<com.alina.mylibrary.model.Category> updateCategory (@RequestBody com.alina.mylibrary.model.Category category);
+
+    @DeleteMapping(path="/{id}")
+    ApiResponse<Boolean> deleteCategory(@PathVariable int id);
+}
