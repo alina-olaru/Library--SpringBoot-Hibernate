6026c06176222dc7663527f3d2641a9634eee818
Alina Olaru <olarualina01@gmail.com>
2020-03-31 17:05:13 +0300

    Complaint Service implementation

--- mylibrary/src/main/java/com/alina/mylibrary/service/impl/ComplaintServiceImp.java
+++ mylibrary/src/main/java/com/alina/mylibrary/service/impl/ComplaintServiceImp.java
@@ -1,4 +1,30 @@
 package com.alina.mylibrary.service.impl;
 
-public class ComplaintServiceImp {
+import com.alina.mylibrary.dao.ComplaintDao;
+import com.alina.mylibrary.model.Complaint;
+import com.alina.mylibrary.service.ComplaintService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class ComplaintServiceImp implements ComplaintService {
+
+    @Autowired
+    ComplaintDao complaintDao;
+
+
+
+    @Override
+    public Complaint addComplaint(Complaint complaint) {
+        if(complaint==null) {
+            return null;
+        }
+        return this.complaintDao.addComplaint(complaint);
+    }
+
+    @Override
+    public List<Complaint> getComplaints() {
+        return this.complaintDao.getComplaints();
+    }
 }
