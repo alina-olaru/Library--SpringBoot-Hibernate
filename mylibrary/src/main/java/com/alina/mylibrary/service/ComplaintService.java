dc3d67649a83ab3e65a848b600cc0f048b6612e1
Alina Olaru <olarualina01@gmail.com>
2020-03-31 17:05:07 +0300

    Complaint Service interface.

--- mylibrary/src/main/java/com/alina/mylibrary/service/ComplaintService.java
+++ mylibrary/src/main/java/com/alina/mylibrary/service/ComplaintService.java
@@ -1,4 +1,11 @@
 package com.alina.mylibrary.service;
 
+import com.alina.mylibrary.model.Complaint;
+
+import java.util.List;
+
 public interface ComplaintService {
+
+    public Complaint addComplaint(Complaint complaint);
+    public List<Complaint> getComplaints();
 }
