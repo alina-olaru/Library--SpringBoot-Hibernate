d0d5b5ecf15f2250623afa596bfcc1768b677998
Alina Olaru <olarualina01@gmail.com>
2020-04-01 20:20:38 +0300

    getQuizzes implemented

--- mylibrary/src/main/java/com/alina/mylibrary/controller/impl/QuizzApiImp.java
+++ mylibrary/src/main/java/com/alina/mylibrary/controller/impl/QuizzApiImp.java
@@ -3,16 +3,25 @@ package com.alina.mylibrary.controller.impl;
 
 import com.alina.mylibrary.controller.QuizzApi;
 import com.alina.mylibrary.model.ApiResponse;
+import com.alina.mylibrary.model.ApiResponseType;
 import com.alina.mylibrary.model.Quizz;
+import com.alina.mylibrary.service.QuizzService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
 public class QuizzApiImp implements QuizzApi {
+
+    @Autowired
+    private QuizzService quizzService;
+
+
+
     @Override
     public ApiResponse<List<Quizz>> getQuizzes() {
-        return null;
+       return new ApiResponse<List<Quizz>>(ApiResponseType.SUCCESS,this.quizzService.getQuizzes(),"s-au adus datele din baza");
     }
 
     @Override
