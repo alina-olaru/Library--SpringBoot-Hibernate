e9a969238fd1bf650132e4fd4ab4b9a6fe6138df
Alina Olaru <olarualina01@gmail.com>
2020-04-01 16:54:20 +0300

    updateVoucher implemented

--- mylibrary/src/main/java/com/alina/mylibrary/service/impl/VoucherServiceImp.java
+++ mylibrary/src/main/java/com/alina/mylibrary/service/impl/VoucherServiceImp.java
@@ -47,6 +47,9 @@ public class VoucherServiceImp implements VoucherService {
 
     @Override
     public Voucher updateVoucher(Voucher voucher) {
-        return null;
+       if(voucher==null){
+           return null;
+       }
+       return this.voucherDao.updateVoucher(voucher);
     }
 }
