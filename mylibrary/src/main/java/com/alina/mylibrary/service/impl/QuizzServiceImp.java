872572cf396e9eaaae299f14509219ca65835c98
Alina Olaru <olarualina01@gmail.com>
2020-04-01 20:12:19 +0300

    QuizzServiceImp created.

---
+++ mylibrary/src/main/java/com/alina/mylibrary/service/impl/QuizzServiceImp.java
@@ -0,0 +1,36 @@
+package com.alina.mylibrary.service.impl;
+
+import com.alina.mylibrary.dao.QuizzDao;
+import com.alina.mylibrary.model.Quizz;
+import com.alina.mylibrary.service.QuizzService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class QuizzServiceImp implements QuizzService {
+
+
+    @Autowired
+    QuizzDao quizzDao;
+
+
+    @Override
+    public List<Quizz> getQuizzes() {
+        return null;
+    }
+
+    @Override
+    public Boolean deleteQquizz(int quizzId) {
+        return null;
+    }
+
+    @Override
+    public Quizz updateQuizz(Quizz quizz) {
+        return null;
+    }
+
+    @Override
+    public Quizz addQuizz(Quizz quizz) {
+        return null;
+    }
+}
