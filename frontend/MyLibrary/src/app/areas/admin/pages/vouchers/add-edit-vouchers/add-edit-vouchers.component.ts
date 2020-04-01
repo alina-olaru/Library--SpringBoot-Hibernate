677b31522eb95881f3d19b9b46b0457675a9363a
Alina Olaru <olarualina01@gmail.com>
2020-04-01 18:51:13 +0300

    submit form with model -->implemented

--- frontend/MyLibrary/src/app/areas/admin/pages/vouchers/add-edit-vouchers/add-edit-vouchers.component.ts
+++ frontend/MyLibrary/src/app/areas/admin/pages/vouchers/add-edit-vouchers/add-edit-vouchers.component.ts
@@ -1,4 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from "@angular/core";
+import { Author } from "src/app/Models/admin/AuthorModel";
+import {
+  FormControl,
+  FormGroup,
+  FormBuilder,
+  Validators
+} from "@angular/forms";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { Voucher } from 'src/app/Models/admin/VoucherModel';
+
+interface Data {
+  type: string;
+  model: Voucher;
+}
+
+
+
 
 @Component({
   selector: 'app-add-edit-vouchers',
@@ -6,10 +23,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-edit-vouchers.component.scss']
 })
 export class AddEditVouchersComponent implements OnInit {
+  localForm: FormGroup;
 
-  constructor() { }
+  myControl = new FormControl();
+  fileName: string;
+
+
+  constructor(
+    public dialogRef: MatDialogRef<AddEditVouchersComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Data,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+
+    if (this.data.model == null || this.data.model == undefined) {
+      this.data.model = new Voucher();
   }
 
+    this.localForm = this.formBuilder.group({
+    voucherId: [{ value: this.data.model.voucherId, disabled: true }],
+    voucherTitle: [this.data.model.voucherTitle, Validators.required],
+    voucherDescription: [this.data.model.voucherDescription],
+    voucherMaximumUses: [this.data.model.voucherMaximumUses, Validators.required],
+    voucherPrice: [this.data.model.voucherPrice, Validators.required]
+
+  });
+
+}
+get form() {
+  return this.localForm.controls;
+}
+
+
+
+onNoClick(): void {
+  this.dialogRef.close();
+}
+
+
+SubmitForm() {
+  if (this.localForm.valid) {
+    console.log(this.localForm.value);
+    let model: Voucher = new Voucher(this.localForm.value);
+    model.voucherId = this.data.model.voucherId;
+    this.dialogRef.close(model);
+  }
+}
 }
