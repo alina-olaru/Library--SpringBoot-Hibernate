773820f9a13dca44b4de576a034c5be7f63c1b50
Alina Olaru <olarualina01@gmail.com>
2020-04-01 20:49:11 +0300

    string changed to list<string> because angular parses wrong

--- mylibrary/src/main/java/com/alina/mylibrary/model/Quizz.java
+++ mylibrary/src/main/java/com/alina/mylibrary/model/Quizz.java
@@ -1,14 +1,18 @@
 package com.alina.mylibrary.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import java.util.Date;
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 @Entity
 @Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Quizz {
 
     @Id
@@ -27,7 +31,8 @@ public class Quizz {
 
     @NotNull
     @Column(columnDefinition = "NVARCHAR(MAX)")
-    private String quizzAnswers;
+    @ElementCollection
+    private List<String> quizzAnswers=new ArrayList<String>();
 
 
     @NotNull
@@ -44,92 +49,8 @@ public class Quizz {
 
     private Date quizzEndDate;
 
-    public Date getQuizzStartDate() {
-        return quizzStartDate;
-    }
 
-    public void setQuizzStartDate(Date quizzStartDate) {
-        this.quizzStartDate = quizzStartDate;
-    }
 
-    public Date getQuizzEndDate() {
-        return quizzEndDate;
-    }
-
-    public void setQuizzEndDate(Date quizzEndDate) {
-        this.quizzEndDate = quizzEndDate;
-    }
-
-    public Quizz() {
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Quizz quizz = (Quizz) o;
-        return quizzId == quizz.quizzId &&
-                numberOfQuestions == quizz.numberOfQuestions &&
-                quizzQuestion.equals(quizz.quizzQuestion) &&
-                quizzAnswers.equals(quizz.quizzAnswers) &&
-                quizzCorrectAnswer.equals(quizz.quizzCorrectAnswer) &&
-                quizzStartDate.equals(quizz.quizzStartDate) &&
-                quizzEndDate.equals(quizz.quizzEndDate) &&
-                vouchersGotByQuizz.equals(quizz.vouchersGotByQuizz);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(quizzId, numberOfQuestions, quizzQuestion, quizzAnswers, quizzCorrectAnswer, quizzStartDate, quizzEndDate, vouchersGotByQuizz);
-    }
-
-    public int getQuizzId() {
-        return quizzId;
-    }
-
-    public void setQuizzId(int quizzId) {
-        this.quizzId = quizzId;
-    }
-
-    public int getNumberOfQuestions() {
-        return numberOfQuestions;
-    }
-
-    public void setNumberOfQuestions(int numberOfQuestions) {
-        this.numberOfQuestions = numberOfQuestions;
-    }
-
-    public String getQuizzQuestion() {
-        return quizzQuestion;
-    }
-
-    public void setQuizzQuestion(String quizzQuestion) {
-        this.quizzQuestion = quizzQuestion;
-    }
-
-    public String getQuizzAnswers() {
-        return quizzAnswers;
-    }
-
-    public void setQuizzAnswers(String quizzAnswers) {
-        this.quizzAnswers = quizzAnswers;
-    }
-
-    public String getQuizzCorrectAnswer() {
-        return quizzCorrectAnswer;
-    }
-
-    public void setQuizzCorrectAnswer(String quizzCorrectAnswer) {
-        this.quizzCorrectAnswer = quizzCorrectAnswer;
-    }
-
-    public List<Voucher> getVouchersGotByQuizz() {
-        return vouchersGotByQuizz;
-    }
-
-    public void setVouchersGotByQuizz(List<Voucher> vouchersGotByQuizz) {
-        this.vouchersGotByQuizz = vouchersGotByQuizz;
-    }
 
     @OneToMany(mappedBy = "quizzez",
     cascade = CascadeType.ALL)
