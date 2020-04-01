fde8e31c2755d5311addccc83224fee53aa29d0e
Alina Olaru <olarualina01@gmail.com>
2020-04-01 16:56:45 +0300

    VoucherApi interface with methods created

---
+++ mylibrary/src/main/java/com/alina/mylibrary/controller/VoucherApi.java
@@ -0,0 +1,31 @@
+package com.alina.mylibrary.controller;
+
+
+import com.alina.mylibrary.model.ApiResponse;
+import com.alina.mylibrary.model.Category;
+import com.alina.mylibrary.model.Voucher;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/public/api/admin/voucher")
+@CrossOrigin
+public interface VoucherApi {
+
+
+
+    @GetMapping
+    ApiResponse<List<Voucher>> getVouchers();
+
+    @PostMapping
+    ApiResponse<Voucher> insertVoucher(@RequestBody Voucher voucher);
+
+    @PutMapping(path="/{id}")
+    ApiResponse<Voucher> updateVoucher (@RequestBody Voucher voucher);
+
+    @DeleteMapping(path="/{id}")
+    ApiResponse<Boolean> deleteVoucher(@PathVariable int id);
+
+
+}
