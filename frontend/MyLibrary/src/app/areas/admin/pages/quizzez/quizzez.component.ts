58210e694eb272ce6aaf4aaaca810cc32a1a1bc9
Alina Olaru <olarualina01@gmail.com>
2020-04-01 20:49:20 +0300

    imported added

--- frontend/MyLibrary/src/app/areas/admin/pages/quizzez/quizzez.component.ts
+++ frontend/MyLibrary/src/app/areas/admin/pages/quizzez/quizzez.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Injectable, Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { GlobalVarService } from 'src/app/services/global-var.service';
+import { ApiResponse } from 'src/app/Models/general/api-response';
+import { Observable } from 'rxjs';
+import { Quizzez } from 'src/app/Models/admin/QuizzezModel';
 
 @Component({
   selector: 'app-quizzez',
@@ -6,10 +11,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quizzez.component.scss']
 })
 export class QuizzezComponent implements OnInit {
+  baseUrl:String;
 
-  constructor() { }
+  constructor(
 
-  ngOnInit(): void {
+    private http: HttpClient,
+    private globalVarService: GlobalVarService
+
+    )
+     {
+       this.baseUrl="/public/api/admin/Quizz";
+
+     }
+
+  ngOnInit()
+  {
   }
 
+
+
+  GetQuizzez() {
+    return this.http.get<ApiResponse<Quizzez[]>>(
+      this.globalVarService.globalUrl + this.baseUrl
+    );
+  }
+
+  DeleteQuizzez(id: number): Observable<ApiResponse<boolean>> {
+    return this.http.delete<ApiResponse<boolean>>(
+      this.globalVarService.globalUrl + this.baseUrl + "/" + id
+    );
+  }
+
+  AddQuizzez(publisher: Quizzez): Observable<ApiResponse<Quizzez>> {
+    return this.http.post<ApiResponse<Quizzez>>(
+      this.globalVarService.globalUrl + this.baseUrl,
+      publisher
+    );
+  }
+
+  UpdateQuizzez(publisher:Quizzez ,id: number): Observable<ApiResponse<Quizzez>> {
+    return this.http.put<ApiResponse<Quizzez>>(
+      this.globalVarService.globalUrl + this.baseUrl + "/" + id,
+      publisher
+    );
+  }
 }
+
