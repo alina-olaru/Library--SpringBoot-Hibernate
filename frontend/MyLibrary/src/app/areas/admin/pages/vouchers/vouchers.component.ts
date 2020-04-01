fc21d6b5930a4fd53ecb17fa334e0e37c2a72791
Alina Olaru <olarualina01@gmail.com>
2020-04-01 18:36:27 +0300

    methods for actions implemented.

--- frontend/MyLibrary/src/app/areas/admin/pages/vouchers/vouchers.component.ts
+++ frontend/MyLibrary/src/app/areas/admin/pages/vouchers/vouchers.component.ts
@@ -1,3 +1,5 @@
+import { AddEditVouchersComponent } from './add-edit-vouchers/add-edit-vouchers.component';
+import { Voucher } from 'src/app/Models/admin/VoucherModel';
 import { MatTableDataSource } from '@angular/material/table';
 import { TitleService } from './../../services/title.service';
 import { Component, OnInit, ViewChild } from '@angular/core';
@@ -20,7 +22,7 @@ import { VoucherService } from './voucher.service';
 })
 export class VouchersComponent implements OnInit {
   @ViewChild(MatSort, { static: false }) sort: MatSort;
-  @ViewChild(MatPaginatoraginator, { static: false }) paginator: MatPaginator;
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
 
 
 
@@ -87,23 +89,159 @@ dataSource:MatTableDataSource<Voucher>=new MatTableDataSource(this.vouchers);
 
     this.voucherService
     .GetVouchers()
+    .subscribe((response : ApiResponse<Voucher[]>) => {
+      this.loadingService.stop();
+
+      if(response && response.status==ApiResponseType.SUCCESS){
+
+        if(response.body.length==0){
+          this.toastr.Toast.fire({
+            icon: 'info',
+            title: 'Nu exista vouchere in baza de date'
+          });
+        }
+        this.vouchers=response.body;
+        this.updateDataSouce();
+      }
+    });
 
 
   }
+
 
 
 
   AddVoucher(){
+    const dialogRef = this.dialog.open(AddEditVouchersComponent, {
+      width: '400px',
+      data: {
+        type: 'add',
+        model: this._addVoucher
+      }
+    });
 
+    dialogRef.afterClosed().subscribe(result => {
+      if (result != undefined && result != null) {
+        this.AddVoucherrConfirm(result);
+      }
+    });
 
   }
 
-  UpdateVoucher(){
 
+  AddVoucherrConfirm(voucher:Voucher){
+    this.loadingService.start();
+    this.voucherService.AddVoucher(voucher)
+    .subscribe((response:ApiResponse<Voucher>) => {
+      this.loadingService.stop();
+
+      if (response && response.status == ApiResponseType.SUCCESS) {
+
+        this.toastr.Toast.fire({
+          title: 'Voucherul a fost adaugat cu succes',
+          icon: 'success'
+        });
+        this.vouchers.push(response.body);
+        this.updateDataSouce();
+      } else {
+        this.toastr.Swal.fire(
+          'Eroare!',
+          'A aparut o eroare la adaugare, incearca din nou!',
+          'error'
+        );
+      }
+    },
+    error => {
+      this.toastr.Swal.fire(
+        'Eroare!',
+        'A aparut o eroare la adaugare, incearca din nou!',
+        'error'
+      );
+    }
+  );
+}
+
+
+  UpdateVoucher(item:Voucher){
+    const dialogRef = this.dialog.open(AddEditVouchersComponent, {
+      width: '400px',
+      data: {
+        type: 'edit',
+        model: Object.assign({}, item)
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result != undefined && result != null) {
+        this.EditVoucherConfirm(result, item);
+      }
+    });
   }
 
-  DeleteVoucher(){
+  EditVoucherConfirm(newVoucher: Voucher, oldVoucher: Voucher) {
+    this.loadingService.start();
 
+    this.voucherService
+      .UpdateVoucher(newVoucher, newVoucher.voucherId)
+      .subscribe((response: ApiResponse<Voucher>) => {
+        this.loadingService.stop();
+        if (response && response.status == ApiResponseType.SUCCESS) {
+          this.toastr.Toast.fire({
+            title: 'Vouchcerul a fost editat cu succes!',
+            icon: 'success'
+          });
+
+          const idxOld = this.vouchers.indexOf(oldVoucher);
+          this.vouchers[idxOld] = newVoucher;
+          this.updateDataSouce();
+        } else {
+          this.toastr.Swal.fire(
+            'Eroare!',
+            'A aparut o eroare la editare, incearca din nou!',
+            'error'
+          );
+        }
+      });
+  }
+
+  DeleteVoucher(voucher:Voucher){
+    this.toastr.Swal.fire({
+      title: 'Esti sigur ca vrei sa stergi acest bautura?',
+      html: `Id: <b>${voucher.voucherId}</b> - Nume: <b>${voucher.voucherTitle}</b>`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Da',
+      cancelButtonText: 'Nu'
+    }).then(result => {
+      if (result.value) {
+        this.loadingService.start();
+
+        this.voucherService
+          .DeleteVoucher(voucher.voucherId)
+          .subscribe((response: ApiResponse<boolean>) => {
+            this.loadingService.stop();
+            if (
+              response &&
+              response.status == ApiResponseType.SUCCESS &&
+              response.body == true
+            ) {
+              this.toastr.Toast.fire({
+                title: 'Voucherul a fost sters.',
+                icon: 'success'
+              });
+              this.GetVoucher();
+            } else {
+              this.toastr.Swal.fire(
+                'Eroare!',
+                'A aparut o eroare la stergere, incearca din nou!',
+                'error'
+              );
+            }
+          });
+      }
+    });
   }
 
 
