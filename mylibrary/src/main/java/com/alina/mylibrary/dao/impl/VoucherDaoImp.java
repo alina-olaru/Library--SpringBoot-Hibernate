e011bdfc4a900332904202135d1af54c45cd05f4
Alina Olaru <olarualina01@gmail.com>
2020-04-01 16:44:56 +0300

    get all vouchers

--- mylibrary/src/main/java/com/alina/mylibrary/dao/impl/VoucherDaoImp.java
+++ mylibrary/src/main/java/com/alina/mylibrary/dao/impl/VoucherDaoImp.java
@@ -1,4 +1,40 @@
 package com.alina.mylibrary.dao.impl;
 
-public interface VoucherDao {
+import com.alina.mylibrary.dao.VoucherDao;
+import com.alina.mylibrary.model.Voucher;
+import com.alina.mylibrary.repository.VoucherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+
+@Component
+public class VoucherDaoImp implements VoucherDao {
+
+
+    @Autowired
+    private VoucherRepository voucherRepository;
+
+
+
+    @Override
+    public List<Voucher> getVouchers() {
+        return this.voucherRepository.findAll();
+    }
+
+    @Override
+    public Voucher addVoucher(Voucher voucher) {
+        return null;
+    }
+
+    @Override
+    public Voucher updateVoucher(Voucher voucher) {
+        return null;
+    }
+
+    @Override
+    public Boolean deleteVoucher() {
+        return null;
+    }
 }
