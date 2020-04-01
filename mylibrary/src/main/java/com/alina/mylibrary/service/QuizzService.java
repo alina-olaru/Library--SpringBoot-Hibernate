1cd783a3902563dd401fb7ee8092bd65d61d0224
Alina Olaru <olarualina01@gmail.com>
2020-04-01 20:11:36 +0300

    QuizzService interface declared (with CRUD methods )

---
+++ mylibrary/src/main/java/com/alina/mylibrary/service/QuizzService.java
@@ -0,0 +1,17 @@
+package com.alina.mylibrary.service;
+
+
+import com.alina.mylibrary.model.Quizz;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface QuizzService {
+    public List<Quizz> getQuizzes();
+    public Boolean deleteQquizz(int quizzId);
+    public Quizz updateQuizz(Quizz quizz);
+    public Quizz addQuizz(Quizz quizz);
+
+}
