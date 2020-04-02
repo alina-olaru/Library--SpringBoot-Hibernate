45d48edd3f2d162895918162d2837df8e54c2b9c
Alina Olaru <olarualina01@gmail.com>
2020-04-02 13:55:58 +0300

    interface for user

---
+++ mylibrary/src/main/java/com/alina/mylibrary/controller/BookUserApi.java
@@ -0,0 +1,28 @@
+package com.alina.mylibrary.controller;
+
+import com.alina.mylibrary.model.ApiResponse;
+import com.alina.mylibrary.model.Author;
+import com.alina.mylibrary.model.BookUser;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping("/public/api/admin/users")
+@CrossOrigin
+public interface BookUserApi {
+
+
+    @PostMapping
+    ApiResponse<BookUser> insertUser(@RequestBody BookUser bookUser);
+
+
+    @GetMapping
+    ApiResponse<List<BookUser>>  getUser();
+
+
+    @PutMapping(path="/{id}")
+    ApiResponse<BookUser> updateUser(@RequestBody BookUser bookUser);
+
+    @DeleteMapping(path="/{id}")
+    ApiResponse<Boolean> deleteUser(@PathVariable int id);
+}
