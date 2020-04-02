aa9f791bec2a5bc34970078539d1f21f7468994a
Alina Olaru <olarualina01@gmail.com>
2020-04-02 14:20:22 +0300

    services implementation

--- mylibrary/src/main/java/com/alina/mylibrary/controller/impl/BookUserApiImp.java
+++ mylibrary/src/main/java/com/alina/mylibrary/controller/impl/BookUserApiImp.java
@@ -1,4 +1,48 @@
 package com.alina.mylibrary.controller.impl;
 
-public class BookUserApiImp {
+
+import com.alina.mylibrary.controller.BookUserApi;
+import com.alina.mylibrary.model.ApiResponse;
+import com.alina.mylibrary.model.ApiResponseType;
+import com.alina.mylibrary.model.BookUser;
+import com.alina.mylibrary.service.BookUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class BookUserApiImp implements BookUserApi {
+
+
+    @Autowired
+    BookUserService bookUserService;
+
+
+
+    @Override
+    public ApiResponse<BookUser> insertUser(BookUser bookUser) {
+        if(bookUser==null){
+            return new ApiResponse<BookUser>(ApiResponseType.ERROR,null,"nu s-a putut adauga userul in baza de date");
+
+        }
+        return new ApiResponse<BookUser>(ApiResponseType.SUCCESS,this.bookUserService.addUser(bookUser));
+
+
+    }
+
+    @Override
+    public ApiResponse<List<BookUser>> getUser() {
+        return new ApiResponse<List<BookUser>>(ApiResponseType.SUCCESS,this.bookUserService.getUsers(),"s-au adus datele cu succces)";
+    }
+
+    @Override
+    public ApiResponse<BookUser> updateUser(BookUser bookUser) {
+        return null;
+    }
+
+    @Override
+    public ApiResponse<Boolean> deleteUser(int id) {
+        return null;
+    }
 }
--- mylibrary/src/main/java/com/alina/mylibrary/service/impl/BookUserServiceImpl.java
+++ mylibrary/src/main/java/com/alina/mylibrary/service/impl/BookUserServiceImpl.java
@@ -6,6 +6,8 @@ import com.alina.mylibrary.service.BookUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BookUserServiceImpl implements BookUserService {
 
@@ -22,5 +24,31 @@ public class BookUserServiceImpl implements BookUserService {
             user.setPassword(null);
         }
         return user;
+    }
+
+    @Override
+    public List<BookUser> getUsers() {
+        return this.bookUserDao.getBookUsers();
+    }
+
+    @Override
+    public BookUser addUser(BookUser bookUser) {
+        if(bookUser==null){
+            return null;
+        }
+        return this.bookUserDao.addBookUser(bookUser);
+    }
+
+    @Override
+    public BookUser editUser(BookUser bookUser) {
+        if(bookUser==null){
+            return null;
+        }
+        return this.bookUserDao.updateBookUser(bookUser);
+    }
+
+    @Override
+    public Boolean deleteUser(int id) {
+        return this.bookUserDao.removeBookUser(id);
     }
 }
