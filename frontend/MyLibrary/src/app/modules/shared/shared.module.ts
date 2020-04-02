da90e89316937030eafec758439fe076cb80198d
Alina Olaru <olarualina01@gmail.com>
2020-04-02 15:26:11 +0300

    pipe YesNo added

--- frontend/MyLibrary/src/app/modules/shared/shared.module.ts
+++ frontend/MyLibrary/src/app/modules/shared/shared.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TruncatePipe } from '../pipes/truncate.pipe';
-
+import { YesNoPipe } from '../pipes/yes-no.pipe';
 
 
 @NgModule({
-  declarations: [TruncatePipe],
+  declarations: [TruncatePipe,YesNoPipe],
   imports: [
     CommonModule,
   ],
-  exports: [TruncatePipe]
+  exports: [TruncatePipe,YesNoPipe]
 })
 export class SharedModule { }
