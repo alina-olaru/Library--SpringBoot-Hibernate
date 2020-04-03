13d4ff7b9606a9d25b5ccea9c68dba12b7e721c6
Alina Olaru <olarualina01@gmail.com>
2020-04-03 12:32:18 +0300

    ConfirmationToken

--- mylibrary/src/main/java/com/alina/mylibrary/model/ConfirmationToken.java
+++ mylibrary/src/main/java/com/alina/mylibrary/model/ConfirmationToken.java
@@ -1,4 +1,39 @@
 package com.alina.mylibrary.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ConfirmationToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    private long tokenid;
+
+    @Column
+    private String confirmationToken;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @OneToOne(targetEntity = BookUser.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "userId")
+    private BookUser user;
+
+    public ConfirmationToken(BookUser user) {
+        this.user = user;
+        createdDate = new Date();
+        confirmationToken = UUID.randomUUID().toString();
+    }
+
+    // getters and setters
 }
