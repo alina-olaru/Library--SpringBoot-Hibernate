6b6dc6c4d063258c19f3da7d97b5f5d0f7f05536
Alina Olaru <olarualina01@gmail.com>
2020-04-06 16:39:31 +0300

    icons

--- frontend/MyLibrary/src/app/areas/Home/welcome/welcome.component.ts
+++ frontend/MyLibrary/src/app/areas/Home/welcome/welcome.component.ts
@@ -1,25 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { SliderComponent} from '../slider/slider.component';
+import { Component, OnInit } from "@angular/core";
+import { SliderComponent } from "../slider/slider.component";
 import {
-faUser,
-faShoppingCart,
-faBookOpen
-} from "@fortawesome/free-solid-svg-icons";@Component({
-  selector: 'app-welcome',
-  templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
+  faUser,
+  faShoppingCart,
+  faBookOpen,
+  faGift,
+  faCarSide,
+  faBiking,
+} from "@fortawesome/free-solid-svg-icons";
+@Component({
+  selector: "app-welcome",
+  templateUrl: "./welcome.component.html",
+  styleUrls: ["./welcome.component.scss"],
 })
 export class WelcomeComponent implements OnInit {
+  faUser = faUser;
+  faShoppingCart = faShoppingCart;
+  faBookOpen = faBookOpen;
+  faGiftfaGift = faGift;
+  faCarSide = faCarSide;
+  faBiking = faBiking;
 
-  faUser=faUser;
-  faShoppingCart=faShoppingCart;
-  faBookOpen=faBookOpen;
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
-
+  ngOnInit(): void {}
 }
 
 /*
