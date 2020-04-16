56a272ff312d4c5ab024b67b6bc5c68efeab5f61
Alina Olaru <olarualina01@gmail.com>
2020-04-16 15:23:26 +0300

    fixing test

--- mylibrary/src/test/java/com/alina/mylibrary/AuthorTest.java
+++ mylibrary/src/test/java/com/alina/mylibrary/AuthorTest.java
@@ -3,18 +3,25 @@ package com.alina.mylibrary;
 
 import com.alina.mylibrary.controller.AuthorApi;
 import com.alina.mylibrary.repository.AuthorRepository;
+import javafx.application.Application;
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static javax.swing.UIManager.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,7 +34,9 @@ import org.springframework.web.context.WebApplicationContext;
 
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(value = AuthorApi.class)
+@WebMvcTest(AuthorApi.class)
+@SpringBootTest
+
 public class AuthorTest {
 
 
@@ -47,9 +56,13 @@ public class AuthorTest {
 
     @Test
     public void getAuthors() throws Exception{
-        this.mockMvc.perform(get("/api/author"))
-                .andExpect(status().is2xxSuccessful())
-                .andDo(print());
+        this.mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/author")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+//                .andExpect(MockMvcResultMatchers.jsonPath("$.author").exists())
+//                .andExpect(MockMvcResultMatchers.jsonPath("$.author[*].authorId").isNotEmpty());
 
 
     }
