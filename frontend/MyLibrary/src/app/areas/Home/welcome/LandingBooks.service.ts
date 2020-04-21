b34c8532c8e4768a9e2c465d02eaf5deded3eb48
Alina Olaru <olarualina01@gmail.com>
2020-04-21 16:00:36 +0300

    ang service for SF books

---
+++ frontend/MyLibrary/src/app/areas/Home/welcome/LandingBooks.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { GlobalVarService } from 'src/app/services/global-var.service';
+import { LoadingService } from 'src/app/modules/loading-spinner/loading.service';
+import { MatDialog } from '@angular/material/dialog';
+import { TitleService } from '../../admin/services/title.service';
+import { DomSanitizer } from '@angular/platform-browser';
+import { ToastrService } from 'src/app/services/toastr.service';
+import { ApiResponse } from 'src/app/Models/general/api-response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LandingBooksService {
+
+constructor(
+
+  private http: HttpClient,
+  private globalVarService: GlobalVarService,
+  private loadingService: LoadingService,
+  public dialog: MatDialog,
+  private titleService: TitleService,
+  private sanitizer: DomSanitizer,
+  private toastr: ToastrService
+
+
+)   {
+  this.baseUrl="/public/api/landing/book";
+}
+
+
+GetBooksSF(){
+
+
+  return this.http.get<ApiResponse<Book[]>>(this.globalVarService.globalUrl + this.baseUr+"/SF");
+}
+
+}
