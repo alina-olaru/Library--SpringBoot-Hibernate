f1b940c79be865b22ca64bc42dadad7ccb0ca2ec
Alina Olaru <olarualina01@gmail.com>
2020-04-22 17:54:52 +0300

    chart added

--- frontend/MyLibrary/src/app/areas/admin/pages/dashboard/dashboard.component.ts
+++ frontend/MyLibrary/src/app/areas/admin/pages/dashboard/dashboard.component.ts
@@ -18,6 +18,8 @@ export class DashboardComponent implements OnInit {
   private chart:am4charts.XYChart;
   private chart_second:am4charts.XYChart;
   private chart_third:am4charts.XYChart;
+  private chart_fourth:am4charts.XYChart;
+  private chart_fifth:am4charts.XYChart;
 
 
 
@@ -40,6 +42,7 @@ export class DashboardComponent implements OnInit {
       //AUTOR + CARTI VANDUTE
       //AUTOR + NR CARTI EXISTENTE PE SITE
       //CIFRA AFACERI PE LUNA
+      //la third:autor + carti
       this.zone.runOutsideAngular(() => {
         let chart = am4core.create("chartdiv", am4charts.XYChart);
 
@@ -470,6 +473,181 @@ series.colors.step = 3;
         titleImage.height = 200;
 
       });
+
+      this.zone.runOutsideAngular(() => {
+
+
+        let chart_fourth = am4core.create("chartdiv_fourth", am4charts.XYChart);
+        chart_fourth.data = [
+          { year: "1896", uk: 7, ussr: 0, russia: 0, usa: 20, china: 0 },
+          { year: "1900", uk: 78, ussr: 0, russia: 0, usa: 55, china: 0 },
+          { year: "1904", uk: 2, ussr: 0, russia: 0, usa: 394, china: 0 },
+          { year: "1908", uk: 347, ussr: 0, russia: 0, usa: 63, china: 0 },
+          { year: "1912", uk: 160, ussr: 0, russia: 0, usa: 101, china: 0 },
+          { year: "1916", uk: 0, ussr: 0, russia: 0, usa: 0, china: 0 },
+          { year: "1920", uk: 107, ussr: 0, russia: 0, usa: 193, china: 0 },
+          { year: "1924", uk: 66, ussr: 0, russia: 0, usa: 198, china: 0 },
+          { year: "1928", uk: 55, ussr: 0, russia: 0, usa: 84, china: 0 },
+          { year: "1932", uk: 34, ussr: 0, russia: 0, usa: 181, china: 0 },
+          { year: "1936", uk: 36, ussr: 0, russia: 0, usa: 92, china: 0 },
+          { year: "1940", uk: 0, ussr: 0, russia: 0, usa: 0, china: 0 },
+          { year: "1944", uk: 0, ussr: 0, russia: 0, usa: 0, china: 0 },
+          { year: "1948", uk: 56, ussr: 0, russia: 0, usa: 148, china: 0 },
+          { year: "1952", uk: 31, ussr: 117, russia: 0, usa: 130, china: 0 },
+          { year: "1956", uk: 45, ussr: 169, russia: 0, usa: 118, china: 0 },
+          { year: "1960", uk: 28, ussr: 169, russia: 0, usa: 112, china: 0 },
+          { year: "1964", uk: 28, ussr: 174, russia: 0, usa: 150, china: 0 },
+          { year: "1968", uk: 18, ussr: 188, russia: 0, usa: 149, china: 0 },
+          { year: "1972", uk: 29, ussr: 211, russia: 0, usa: 155, china: 0 },
+          { year: "1976", uk: 32, ussr: 285, russia: 0, usa: 155, china: 0 },
+          { year: "1980", uk: 45, ussr: 442, russia: 0, usa: 0, china: 0 },
+          { year: "1984", uk: 72, ussr: 0, russia: 0, usa: 333, china: 76 },
+          { year: "1988", uk: 53, ussr: 294, russia: 0, usa: 193, china: 53 },
+          { year: "1992", uk: 50, ussr: 0, russia: 0, usa: 224, china: 83 },
+          { year: "1996", uk: 26, ussr: 0, russia: 115, usa: 260, china: 110 },
+          { year: "2000", uk: 55, ussr: 0, russia: 188, usa: 248, china: 79 },
+          { year: "2004", uk: 57, ussr: 0, russia: 192, usa: 264, china: 94 },
+          { year: "2008", uk: 77, ussr: 0, russia: 143, usa: 315, china: 184 }
+        ];
+
+        // Create axes
+        let categoryAxis = chart_fourth.xAxes.push(new am4charts.CategoryAxis());
+        categoryAxis.dataFields.category = "year";
+        categoryAxis.renderer.grid.template.location = 0;
+        categoryAxis.renderer.minGridDistance = 50;
+        categoryAxis.startLocation = 0.5;
+        categoryAxis.endLocation = 0.5;
+
+        let valueAxis = chart_fourth.yAxes.push(new am4charts.ValueAxis());
+
+
+        // Create series
+        function createSeries(field, name) {
+          let series = chart_fourth.series.push(new am4charts.LineSeries());
+          series.dummyData = {
+            field: field
+          }
+          series.dataFields.valueY = field + "_hi";
+          series.dataFields.openValueY = field + "_low";
+          series.dataFields.categoryX = "year";
+          series.name = name;
+          series.tooltipText = "[font-size: 18]{name}[/]\n{categoryX}: [bold]{" + field + "}[/]";
+          series.strokeWidth = 1;
+          series.fillOpacity = 1;
+          series.tensionX = 0.8;
+
+          return series;
+        }
+
+        createSeries("uk", "United Kingdom");
+        createSeries("ussr", "Soviet Union");
+        createSeries("russia", "Russia");
+        createSeries("usa", "United States");
+        createSeries("china", "China");
+
+        // Legend
+        chart_fourth.legend = new am4charts.Legend();
+        chart_fourth.legend.itemContainers.template.togglable = false;
+        chart_fourth.legend.itemContainers.template.cursorOverStyle = am4core.MouseCursorStyle.default;
+        chart_fourth.legend.position = "right"
+        chart_fourth.legend.reverseOrder = true;
+
+        // Cursor
+        chart_fourth.cursor = new am4charts.XYCursor();
+        chart_fourth.cursor.maxTooltipDistance = 0;
+
+        // Responsive
+        chart_fourth.responsive.enabled = true;
+        chart_fourth.responsive.useDefault = false;
+        chart_fourth.responsive.rules.push({
+          relevant: am4core.ResponsiveBreakpoints.widthL,
+          state: function(target, stateId) {
+            if (target instanceof am4charts.Legend) {
+              let state = target.states.create(stateId);
+              state.properties.position = "bottom";
+              return state;
+            }
+            return null;
+          }
+        });
+
+        // Prepare data for the river-stacked series
+        chart_fourth.events.on("beforedatavalidated", updateData);
+        function updateData() {
+
+          let data = chart_fourth.data;
+          if (data.length == 0) {
+            return;
+          }
+
+          for (var i = 0; i < data.length; i++) {
+            let row = data[i];
+            let sum = 0;
+
+            // Calculate open and close values
+            chart_fourth.series.each(function(series) {
+              let field = series.dummyData.field;
+              let val = Number(row[field]);
+              row[field + "_low"] = sum;
+              row[field + "_hi"] = sum + val;
+              sum += val;
+            });
+
+            // Adjust values so they are centered
+            let offset = sum / 2;
+            chart_fourth.series.each(function(series) {
+              let field = series.dummyData.field;
+              row[field + "_low"] -= offset;
+              row[field + "_hi"] -= offset;
+            });
+
+          }
+
+        }
+
+      });
+
+      this.zone.runOutsideAngular(()=>{
+
+        let chart_fifth = am4core.create("chartdiv_fifth", am4charts.XYChart);
+        let data = [];
+let value = 50;
+for(var i = 0; i < 300; i++){
+  let date = new Date();
+  date.setHours(0,0,0,0);
+  date.setDate(i);
+  value -= Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
+  data.push({date:date, value: value});
+}
+
+chart_fifth.data = data;
+
+// Create axes
+let dateAxis = chart_fifth.xAxes.push(new am4charts.DateAxis());
+dateAxis.renderer.minGridDistance = 60;
+
+let valueAxis = chart_fifth.yAxes.push(new am4charts.ValueAxis());
+
+// Create series
+let series = chart_fifth.series.push(new am4charts.LineSeries());
+series.dataFields.valueY = "value";
+series.dataFields.dateX = "date";
+series.tooltipText = "{value}"
+
+series.tooltip.pointerOrientation = "vertical";
+
+chart_fifth.cursor = new am4charts.XYCursor();
+chart_fifth.cursor.snapToSeries = series;
+chart_fifth.cursor.xAxis = dateAxis;
+
+//chart.scrollbarY = new am4core.Scrollbar();
+chart_fifth.scrollbarX = new am4core.Scrollbar();
+
+
+
+      });
+
+
     }
 
     ngOnDestroy() {
@@ -483,6 +661,9 @@ series.colors.step = 3;
 
         if (this.chart_third) {
           this.chart_third.dispose();
+        }
+        if (this.chart_fourth) {
+          this.chart_fourth.dispose();
         }
       });
     }
