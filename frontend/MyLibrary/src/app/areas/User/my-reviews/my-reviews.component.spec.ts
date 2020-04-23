35bb52c545bd28cb1ef4273626a01225fff8d792
Alina Olaru <olarualina01@gmail.com>
2020-04-23 12:53:09 +0300

    page added

---
+++ frontend/MyLibrary/src/app/areas/User/my-reviews/my-reviews.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MyReviewsComponent } from './my-reviews.component';
+
+describe('MyReviewsComponent', () => {
+  let component: MyReviewsComponent;
+  let fixture: ComponentFixture<MyReviewsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ MyReviewsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MyReviewsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/MyLibrary/src/app/areas/User/my-reviews/my-reviews.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-reviews',
+  templateUrl: './my-reviews.component.html',
+  styleUrls: ['./my-reviews.component.scss']
+})
+export class MyReviewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
