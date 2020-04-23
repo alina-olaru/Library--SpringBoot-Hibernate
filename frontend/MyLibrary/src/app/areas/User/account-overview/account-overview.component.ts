974009b72df88e1f77321782f07563919efc317b
Alina Olaru <olarualina01@gmail.com>
2020-04-23 17:35:56 +0300

    token from cookie

--- frontend/MyLibrary/src/app/areas/User/account-overview/account-overview.component.ts
+++ frontend/MyLibrary/src/app/areas/User/account-overview/account-overview.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { GlobalVarService } from 'src/app/services/global-var.service';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'src/app/services/toastr.service';
+import { BookUser } from 'src/app/Models/BookUser';
 
 @Component({
   selector: 'app-account-overview',
@@ -7,9 +13,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountOverviewComponent implements OnInit {
 
-  constructor() { }
+
+  public user: BookUser = null;
+  private _token: String = null;
+
+
+  constructor(
+
+
+    private gloablVarService: GlobalVarService,
+    private cookieService: CookieService,
+    private router: Router,
+    private toastr: ToastrService
+
+)
+
+  {
+    let cachedUser = this.cookieService.get("auth-user-info");
+    if (cachedUser != null && cachedUser != "") {
+      this.user = JSON.parse(cachedUser) as BookUser;
+      console.log(this.user);
+    }
+
+    let cachedToken = this.cookieService.get("auth-token");
+    if (cachedToken != null && cachedToken != "") {
+      this._token = JSON.parse(cachedToken);
+    }
+
+
+   }
 
   ngOnInit(): void {
   }
 
+
+  GetUser(){
+
+
+  }
 }
