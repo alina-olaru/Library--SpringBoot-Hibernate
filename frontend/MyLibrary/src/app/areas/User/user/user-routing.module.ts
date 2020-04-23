030eba8cca5704bf1f06916d3ae044c4bd0e40f6
Alina Olaru <olarualina01@gmail.com>
2020-04-23 13:09:46 +0300

    Routing added

--- frontend/MyLibrary/src/app/areas/User/user/user-routing.module.ts
+++ frontend/MyLibrary/src/app/areas/User/user/user-routing.module.ts
@@ -1,3 +1,9 @@
+import { RecommendationsComponent } from './../recommendations/recommendations.component';
+import { PersonalDataComponent } from './../personal-data/personal-data.component';
+import { OrderHistoryComponent } from './../order-history/order-history.component';
+import { MyReviewsComponent } from './../my-reviews/my-reviews.component';
+import { MyBooksComponent } from './../my-books/my-books.component';
+import { AddressesBookComponent } from './../addresses-book/addresses-book.component';
 import { AccountOverviewComponent } from './../account-overview/account-overview.component';
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -12,9 +18,32 @@ const routes: Routes = [
   {
     path : "accountOverview",
     component : AccountOverviewComponent
-
   }
-
+,
+{
+  path:"adrese",
+  component:AddressesBookComponent
+},
+{
+  path:"bilbiotecapersonala",
+  component:MyBooksComponent
+},
+{
+  path:"Recenzii",
+  component:MyReviewsComponent
+},
+{
+  path:"istoric",
+  component:OrderHistoryComponent
+},
+{
+  path:"datepersonale",
+  component:PersonalDataComponent
+},
+{
+path:"recomandari",
+component:RecommendationsComponent
+}
   // {
   //   path: "register",
   //   component: RegisterComponent
