a4ef9201b6797be4b4ae39955701fc602eed2f7f
Alina Olaru <olarualina01@gmail.com>
2020-04-24 16:11:28 +0300

    model

--- frontend/MyLibrary/src/app/areas/User/addresses-book/add-address/add-address.component.ts
+++ frontend/MyLibrary/src/app/areas/User/addresses-book/add-address/add-address.component.ts
@@ -1,7 +1,18 @@
 import { AddressServiceService } from './../AddressService.service';
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'src/app/services/toastr.service';
+import { Address } from 'src/app/Models/Address';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+
+
+interface Data {
+  type: string;
+  model: Address;
+}
+
+
 
 @Component({
   templateUrl: './add-address.component.html',
@@ -13,6 +24,8 @@ export class AddAddressComponent implements OnInit {
 
 
   constructor(
+    public dialogRef: MatDialogRef<AddAddressComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Data,
     private formBuilder: FormBuilder,
     private toastr: ToastrService,
     private addAddress:AddressServiceService
@@ -21,10 +34,43 @@ export class AddAddressComponent implements OnInit {
   ngOnInit(): void
    {
 
+    if (this.data.model == null || this.data.model == undefined) {
+      this.data.model = new Address();
+    }
+
     this.localForm=this.formBuilder.group({
+      addressId: [{ addressId: this.data.model.addressId, disabled: true }],
+      country: [this.data.model.country, Validators.required],
+      province: [this.data.model.province, Validators.required],
+      city: [this.data.model.city, Validators.required],
+      streetName: [this.data.model.streetName, Validators.required],
+      streetNumber: [this.data.model.streetNumber, Validators.required],
+      floor: [this.data.model.floor, Validators.required],
+      block: [this.data.model.block, Validators.required],
+      appartmentNumber: [this.data.model.appartmentNumber, Validators.required],
+      userAddress: [this.data.model.userAddress, Validators.required],
 
 
-    })
+
+
+
+    });
+
+
   }
+
+  get form() {
+    return this.localForm.controls;
+  }
+  SubmitForm() {
+    console.log(this.localForm);
+    if (this.localForm.valid) {
+      console.log(this.localForm.value);
+      let model: Address = new Address(this.localForm.value);
+      model.addressId = this.data.model.addressId;
+      this.dialogRef.close(model);
+    }
+  }
+
 
 }
