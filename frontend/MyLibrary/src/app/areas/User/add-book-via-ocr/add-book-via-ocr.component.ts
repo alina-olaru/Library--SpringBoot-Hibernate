37e1d8151230a7f25896d26be34eef06ac93fce2
Alina Olaru <olarualina01@gmail.com>
2020-04-27 16:35:01 +0300

    video added

--- frontend/MyLibrary/src/app/areas/User/add-book-via-ocr/add-book-via-ocr.component.ts
+++ frontend/MyLibrary/src/app/areas/User/add-book-via-ocr/add-book-via-ocr.component.ts
@@ -1,26 +1,91 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  Renderer2,
+} from "@angular/core";
 
 @Component({
-  selector: 'app-add-book-via-ocr',
-  templateUrl: './add-book-via-ocr.component.html',
-  styleUrls: ['./add-book-via-ocr.component.scss']
+  selector: "app-add-book-via-ocr",
+  templateUrl: "./add-book-via-ocr.component.html",
+  styleUrls: ["./add-book-via-ocr.component.scss"],
 })
 export class AddBookViaOCRComponent implements OnInit {
+  state: number;
+  @ViewChild("video", { static: true }) videoElement: ElementRef;
+  @ViewChild("canvas", { static: true }) canvas: ElementRef;
 
-  state:number;
+  videoWidth = 100;
+  videoHeight = 100;
+  constraints = {
+    video: {
+      facingMode: "environment",
+      width: { ideal: 4096 },
+      height: { ideal: 2160 },
+    },
+  };
 
-
-  constructor() {
-    this.state=0;
-
+  constructor(private renderer: Renderer2) {
+    this.state = 0;
   }
 
   ngOnInit(): void {
+    console.log(this.state + " state");
+  }
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.state = 0;
+    console.log(this.state + " state");
   }
 
+  startCamera() {
+    console.log(this.state + " state");
+    this.state = 1;
+    console.log(this.state + " state");
+    if (!!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)) {
+      navigator.mediaDevices
+        .getUserMedia(this.constraints)
+        .then(this.attachVideo.bind(this))
+        .catch(this.handleError);
+    } else {
+      alert("Sorry, camera not available.");
+    }
+  }
 
-  openCamera(){
-    this.state=1;
-    console.log("Camera will open");
+  attachVideo(stream) {
+    console.log(this.state + " state");
+    this.renderer.setProperty(
+      this.videoElement.nativeElement,
+      "srcObject",
+      stream
+    );
+    this.renderer.listen(this.videoElement.nativeElement, "play", (event) => {
+      this.videoHeight = this.videoElement.nativeElement.videoHeight;
+      this.videoWidth = this.videoElement.nativeElement.videoWidth;
+    });
+  }
+
+  capture() {
+    console.log(this.state + " state");
+    this.renderer.setProperty(
+      this.canvas.nativeElement,
+      "width",
+      this.videoWidth
+    );
+    this.renderer.setProperty(
+      this.canvas.nativeElement,
+      "height",
+      this.videoHeight
+    );
+    this.canvas.nativeElement
+      .getContext("2d")
+      .drawImage(this.videoElement.nativeElement, 0, 0);
+  }
+
+  handleError(error) {
+    console.error("EROAREEEEEEEEEEEEEEEEEE");
+    console.log("Error: ", error);
   }
 }
