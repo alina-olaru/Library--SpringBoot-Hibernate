d7bc8b2ab34415738aea31d8c902f5d3d5414013
Alina Olaru <olarualina01@gmail.com>
2020-04-27 16:03:32 +0300

    responsive

--- frontend/MyLibrary/src/app/areas/User/my-books/my-books.component.ts
+++ frontend/MyLibrary/src/app/areas/User/my-books/my-books.component.ts
@@ -1,4 +1,7 @@
+import { AddBookViaOCRComponent } from './../add-book-via-ocr/add-book-via-ocr.component';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-my-books',
@@ -8,14 +11,63 @@ import { Component, OnInit } from '@angular/core';
 export class MyBooksComponent implements OnInit {
 
   UrlOCR:string;
-  constructor() {
+  width:number;
+  heigth:number;
+  isSmallScreen:Boolean;
+  isLargeScreen:Boolean;
+  isMediumeScreen:Boolean;
+  x:string;
+  y:string;
+
+
+  constructor(
+    public dialog: MatDialog,
+    public breakpointObserver:BreakpointObserver
+  ) {
     this.UrlOCR="https://api.ocr.space/parse/image";
+    this.isSmallScreen = breakpointObserver.isMatched('(max-width: 599px)');
+    this.isLargeScreen = breakpointObserver.isMatched('(min-width: 1000px)');
+    this.isMediumeScreen = breakpointObserver.isMatched('(min-width: 600px)');
+
+
    }
 
   ngOnInit(): void {
+
+    if(this.isLargeScreen==true){
+
+      this.width=800;
+      this.heigth=600;
+    }
+
+    if(this.isSmallScreen==true){
+
+      this.width=300;
+      this.heigth=400;
+    }
+    if((this.isMediumeScreen==true)&&(this.isLargeScreen==false)){
+
+      this.width=350;
+      this.heigth=440;
+    }
   }
 
   AddBook(){
+    console.log(this.heigth +  "  "+ this.width);
+    this.x=this.width+"px";
+    this.y=this.heigth+"px";
+    console.log(this.x);
+
+    console.log(this.y);
+    const dialogRef = this.dialog.open(AddBookViaOCRComponent, {
+      width: this.x,
+      height: this.y
+      // data: {
+      //   type: 'edit',
+      //   model: Object.assign({}, item)
+      // }
+    });
+
 
   }
 }
