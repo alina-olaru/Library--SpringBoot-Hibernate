137a4798e46c8f82fed12de181e1ec5ff7821129
Alina Olaru <olarualina01@gmail.com>
2020-05-08 18:43:00 +0300

    custom query for dashboard

--- mylibrary/src/main/java/com/alina/mylibrary/model/dashboard/CategoryNumberBooks.java
+++ mylibrary/src/main/java/com/alina/mylibrary/model/dashboard/CategoryNumberBooks.java
@@ -4,6 +4,8 @@ package com.alina.mylibrary.model.dashboard;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Subselect;
+import org.hibernate.annotations.Synchronize;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.Column;
@@ -14,10 +16,38 @@ import javax.persistence.Table;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+
 public class CategoryNumberBooks {
 
-    private Integer numberBooksforCategory;
+
+
+    @Column(name="titleOfCategory")
     private String titleOfCategory;
+
+    @Column(name="numberBooksforCategory")
+    private Integer numberBooksforCategory;
 
 
 }
+
+//
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Entity
+//@Subselect("select c.categoryTitle as numberBooksforCategory ,count(c.categoryId) as titleOfCategory \n" +
+//        "from Category  c \n" +
+//        "join BooksCategories bc\n" +
+//        "on c.categoryId=bc.categories.categoryId \n" +
+//        "group by c.categoryTitle")
+//@Synchronize({"Book","BooksCategories"})
+//public class CategoryNumberBooks {
+//
+//
+//    private Integer numberBooksforCategory;
+//
+//    @Id
+//    private String titleOfCategory;
+//
+//
+//}
--- mylibrary/src/main/java/com/alina/mylibrary/repository/Custom/DashboardRepository.java
+++ mylibrary/src/main/java/com/alina/mylibrary/repository/Custom/DashboardRepository.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.QueryTimeoutException;
+import javax.persistence.TypedQuery;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,14 +24,48 @@ public class DashboardRepository {
 
 
     public List<CategoryNumberBooks> getCategoriesWithNumberBooks() throws QueryCustomException{
+//        List<CategoryNumberBooks> response=null;
+//try {
+//    TypedQuery<CategoryNumberBooks> query = entityManager.createQuery("select c.categoryTitle as titleOfCategory  ,count(c.categoryId) as numberBooksforCategory \n" +
+//            "from Category  c \n" +
+//            "join BooksCategories bc\n" +
+//            "on c.categoryId=bc.categories.categoryId \n" +
+//            "group by c.categoryTitle", CategoryNumberBooks.class);
+//
+//    response = query.getResultList(); //no type warning
+//    return response;
+//
+//}catch (StackOverflowError e){
+//            throw  new QueryCustomException("Nu s-a putut sterge cartea,au aparut probleme in primul query","StackOverflowError","finding books writted only by this author",response.getClass().getName(),"get");
+//        }
+//        catch (QueryTimeoutException ex){
+//            throw  new QueryCustomException("Nu s-a putut sterge cartea,au aparut probleme in primul query" + ex.getMessage(),"QueryTimeoutException","finding books writted only by this author",response.getClass().getName(),"get");
+//
+//        }
+//        catch (QueryException ex) {
+//            throw new QueryCustomException("Nu s-a putut sterge cartea,au aparut probleme in primul query" + ex.getMessage(), "QueryException", "finding books writted only by this author", response.getClass().getName(), "get");
+//        }
+//
+//        catch(QueryCreationException ex){
+//            throw  new QueryCustomException("Nu s-a putut sterge cartea,au aparut probleme in primul query" + ex.getMessage(),"QueryCreationException","finding books writted only by this author",response.getClass().getName(),"get");
+//
+//        }
+//        catch (Exception ex){
+//            throw  new QueryCustomException("Nu s-a putut sterge cartea,au aparut probleme in primul query" + ex.getMessage(),"Exception","finding books writted only by this author",response.getClass().getName(),"get");
+//
+//        }
+//
+//
         List<CategoryNumberBooks> response=null;
         try{
             response=entityManager
-                    .createQuery("select c.categoryTitle as numberBooksforCategory ,count(c.categoryId) as titleOfCategory \n" +
+                    .createQuery("select c.categoryTitle as titleOfCategory, COALESCE(count(c.categoryId),0) as numberBooksforCategory \n" +
                     "from Category  c \n" +
                     "join BooksCategories bc\n" +
                     "on c.categoryId=bc.categories.categoryId \n" +
                     "group by c.categoryTitle")
+                    .unwrap(org.hibernate.query.Query.class)
+                    .setResultTransformer(Transformers.aliasToBean(CategoryNumberBooks.class))
                     .getResultList();
 
 
