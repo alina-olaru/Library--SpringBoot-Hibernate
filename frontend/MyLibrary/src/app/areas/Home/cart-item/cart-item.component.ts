57820d535486c42750b00b9a97baadf51fc43e29
Alina Olaru <olarualina01@gmail.com>
2020-05-12 09:14:34 +0300

    popup

--- frontend/MyLibrary/src/app/areas/Home/cart-item/cart-item.component.ts
+++ frontend/MyLibrary/src/app/areas/Home/cart-item/cart-item.component.ts
@@ -1,7 +1,16 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { Book } from 'src/app/Models/admin/BookModel';
 import { timeout } from 'rxjs/operators';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'src/app/services/toastr.service';
+import { LandingBooksService } from '../welcome/LandingBooks.service';
+import { DomSanitizer } from '@angular/platform-browser';
+import { LoginService } from '../../login/login.service';
+import { WishlistService } from '../book-details/wishlist.service';
+import { PersonalBookService } from '../book-details/personalBook.service';
+import { CartService } from '../cart/cart.service';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 
 interface Data {
@@ -19,7 +28,18 @@ export class CartItemComponent implements OnInit {
   book : Book=null;
   quantity:number=1;
   constructor(
-    @Inject(MAT_DIALOG_DATA) public data: Data
+    @Inject(MAT_DIALOG_DATA) public data: Data,
+    private route: ActivatedRoute,
+    private toastr: ToastrService,
+    private landingBooksService: LandingBooksService,
+    private domSanitizer: DomSanitizer,
+    private auth: LoginService,
+    private wishService: WishlistService,
+    private router: Router,
+    private personalBookService: PersonalBookService,
+    private cartService: CartService,
+    public dialog: MatDialog,
+    public breakpointObserver:BreakpointObserver
     ) {
       if (this.data.model == null || this.data.model == undefined) {
         this.data.model = new Book();
@@ -42,4 +62,13 @@ export class CartItemComponent implements OnInit {
 
   }
 
+  redirectToCart(){
+
+    this.closePop();
+    this.router.navigate(['/home/cart']);
+  }
+
+  closePop(){
+    this.dialog.closeAll();
+  }
 }
